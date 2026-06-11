a419406a452591dabdec9118b92bc096a391d08f
Sanjeevan <sanjeevan3000@gmail.com>
2026-06-11 09:54:27 +0100

    Clean up journal editor and hide auth setup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lifeledger-v4";
+const CACHE_NAME = "lifeledger-v5";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
