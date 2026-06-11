802e7ce92e19d656c8e47da21b63318edc4f77b5
Sanjeevan <sanjeevan3000@gmail.com>
2026-06-11 10:41:10 +0100

    Improve journal checklist editing

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lifeledger-v6";
+const CACHE_NAME = "lifeledger-v7";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
